d16db9f0dbed70005e2a53d95de086baa7ae58d5
Christof Bachmann <christof.bachmann@gmail.com>
2019-11-07 19:14:27 +0100

    Created Spring Boot Application

---
+++ src/main/java/io/swagslash/gametrackerserver/GametrackerserverApplication.java
@@ -0,0 +1,13 @@
+package io.swagslash.gametrackerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametrackerserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GametrackerserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/swagslash/gametrackerserver/GametrackerserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.swagslash.gametrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametrackerserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
