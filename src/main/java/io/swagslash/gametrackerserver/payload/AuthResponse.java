d0ddcc50924e815afac85fabf2f972ce30ad1c9f
Christof Bachmann <christof.bachmann@gmail.com>
2019-11-19 21:01:55 +0100

    Change: adapt authResponse to requirement

--- src/main/java/io/swagslash/gametrackerserver/payload/AuthResponse.java
+++ src/main/java/io/swagslash/gametrackerserver/payload/AuthResponse.java
@@ -3,9 +3,36 @@ package io.swagslash.gametrackerserver.payload;
 public class AuthResponse {
     private String accessToken;
     private String tokenType = "Bearer";
+    private String username;
+    private int expiresIn;
+    private String email;
 
     public AuthResponse(String accessToken) {
         this.accessToken = accessToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(int expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getAccessToken() {
