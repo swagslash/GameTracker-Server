8f91834c356cb3fb62e770c98e859cc34027997c
Christof Bachmann <christof.bachmann@gmail.com>
2019-11-24 23:52:58 +0100

    Add: DTO for GameData

---
+++ src/main/java/io/swagslash/gametrackerserver/dto/GameDTO.java
@@ -0,0 +1,28 @@
+package io.swagslash.gametrackerserver.dto;
+
+public class GameDTO {
+
+    private String name;
+    private String imageId;
+
+    public GameDTO(String name, String imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(String imageId) {
+        this.imageId = imageId;
+    }
+}
