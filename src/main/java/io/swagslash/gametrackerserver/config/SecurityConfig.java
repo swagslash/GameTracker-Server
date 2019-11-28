8bbc5e8a9f6285cff2c353377ee1d403d8e263b8
Christof Bachmann <christof.bachmann@gmail.com>
2019-11-28 11:29:25 +0100

    Add: UrlProtectionException for Agent Controller

--- src/main/java/io/swagslash/gametrackerserver/config/SecurityConfig.java
+++ src/main/java/io/swagslash/gametrackerserver/config/SecurityConfig.java
@@ -109,7 +109,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js")
                 .permitAll()
-                .antMatchers("/api/auth/**", "/api/oauth2/**")
+                .antMatchers("/api/auth/**", "/api/oauth2/**", "/api/agent/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
