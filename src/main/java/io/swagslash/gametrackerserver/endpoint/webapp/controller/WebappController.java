08efb142aaad24f51bd634299ac7b9f618a06b6b
Christof Bachmann <christof.bachmann@gmail.com>
2019-11-30 23:26:33 +0100

    Change: use request param instead of url param

--- src/main/java/io/swagslash/gametrackerserver/endpoint/webapp/controller/WebappController.java
+++ src/main/java/io/swagslash/gametrackerserver/endpoint/webapp/controller/WebappController.java
@@ -3,10 +3,7 @@ package io.swagslash.gametrackerserver.endpoint.webapp.controller;
 import io.swagslash.gametrackerserver.dto.webapp.GameDTO;
 import io.swagslash.gametrackerserver.service.GameService;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,22 +17,15 @@ public class WebappController {
         this.gameService = gameService;
     }
 
-    @GetMapping("/games")
-    @PreAuthorize("hasRole('USER')")
-    public List<GameDTO> findAll() {
-       return gameService.findAll();
-    }
-
     @GetMapping("/games/user")
     @PreAuthorize("hasRole('USER')")
     public List<GameDTO> findAllByUser() {
         return gameService.findAllByUser();
     }
 
-    @GetMapping("/games/user/{term}")
+    @GetMapping("/games/search")
     @PreAuthorize("hasRole('USER')")
-    public List<GameDTO> findBySearch(@PathVariable String term) {
+    public List<GameDTO> findBySearch(@RequestParam String term) {
         return gameService.findBySearch(term);
     }
-
 }
