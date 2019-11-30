5dc3dd4840178c4131639cffc8d4d5a4d1886be8
Christof Bachmann <christof.bachmann@gmail.com>
2019-11-30 23:25:30 +0100

    Change: GameDTO + Tag

--- src/main/java/io/swagslash/gametrackerserver/dto/webapp/GameDTO.java
+++ src/main/java/io/swagslash/gametrackerserver/dto/webapp/GameDTO.java
@@ -1,16 +1,34 @@
 package io.swagslash.gametrackerserver.dto.webapp;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * DTO for delivering data to Frontend
  */
 public class GameDTO {
 
     private String name;
+
     private String imageId;
+
+    private List<GameDTO> genres;
+
+    private List<GameDTO> gamemodes;
 
     public GameDTO(String name, String imageId) {
         this.name = name;
         this.imageId = imageId;
+        this.gamemodes = new ArrayList<>();
+        this.genres = new ArrayList<>();
+    }
+
+    public GameDTO(String name, String imageId, List<GameDTO> genres, List<GameDTO> gamemodes) {
+        this.name = name;
+        this.imageId = imageId;
+        this.genres = genres;
+        this.gamemodes = gamemodes;
     }
 
     public String getName() {
@@ -27,5 +45,21 @@ public class GameDTO {
 
     public void setImageId(String imageId) {
         this.imageId = imageId;
+    }
+
+    public List<GameDTO> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GameDTO> genres) {
+        this.genres = genres;
+    }
+
+    public List<GameDTO> getGamemodes() {
+        return gamemodes;
+    }
+
+    public void setGamemodes(List<GameDTO> gamemodes) {
+        this.gamemodes = gamemodes;
     }
 }
--- src/main/java/io/swagslash/gametrackerserver/model/Tag.java
+++ src/main/java/io/swagslash/gametrackerserver/model/Tag.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Combines IGDBGameMode and IGDBGenre for better handling
+ */
 @Entity
 @Table(name = "Tag")
 public class Tag extends TagMS {
